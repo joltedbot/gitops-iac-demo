23b7200d3807272bb4b10414c1276029f9edb7bf
Dave White <joltedbot@gmail.com>
2025-04-01 18:50:52 -0700

    Fixed issue with typo in ip allocation directive

--- modules/public_ip/main.tf
+++ modules/public_ip/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_public_ip" "vnic_ip" {
   location            = var.region
   resource_group_name = var.resource_group
   allocation_method   = var.allocation_method
-  
+
   tags = {
     Owner    = var.tag-owner
     Project  = var.tag-project
