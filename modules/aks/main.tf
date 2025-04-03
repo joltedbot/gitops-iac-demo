beefe7c1436ba8ca414c75aab8401c54d3dedfb5
Dave White <joltedbot@gmail.com>
2025-04-03 14:19:07 -0700

    Removed the subnet-id from the cluster as it was causing the build to fail.

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -5,10 +5,9 @@ resource "azurerm_kubernetes_cluster" "lab-cluster" {
   dns_prefix          = "ionlab"
 
   default_node_pool {
-    name          = "default"
-    node_count    = 1
-    vm_size       = "Standard_D2_v2"
-    pod_subnet_id = var.subnet
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
   }
 
   identity {
