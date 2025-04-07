671f50414d5a52e0264d009b3ccaafef2c7c77cb
Dave White <joltedbot@gmail.com>
2025-04-07 13:00:34 -0700

    Fixed issue with clouflare provider

--- header.tf
+++ header.tf
@@ -12,20 +12,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=4.25.0"
     }
-
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5"
+      version = "5.2.0"
     }
-
   }
 
 }
 
-
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
-}
 
 provider "azurerm" {
   client_id       = var.client_id
@@ -33,4 +27,8 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
   features {}
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
